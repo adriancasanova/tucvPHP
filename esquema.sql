447e6b4d0f1b2c6df7d069d1b49b94d6e9a7b15b
adriancasanova <39831043+adriancasanova@users.noreply.github.com>
2022-05-26 21:37:45 -0300

    Delete esquema.sql

--- esquema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `tucv` (
-  `id` int(10) UNSIGNED NOT NULL primary key AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL,
-  `asunto` varchar(255) NOT NULL,
-  `cuerpoEmail` varchar(5000) DEFAULT NULL,
-  `cv` tinyblob NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
